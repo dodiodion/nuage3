b11eb9bcaf48ccdc3d80091e713ccf64445813b7
DominiqueDion <dodiodion@gmail.com>
2018-11-28 11:48:31 -0500

    final commit

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import http.client
 
-connection = http.client.HTTPConnection("132.207.12.228", 8080)
+connection = http.client.HTTPConnection("132.207.12.97", 8080)
 connection.request("GET", "/")
 response = connection.getresponse()
 print(response.read())
--- test_performance.py
+++ test_performance.py
@@ -2,18 +2,55 @@ import http.client, random, sys, time
 from queue import Queue
 from threading import Thread
 
+# 1
+# simple : 0.5062289237976074
+# load balanced : 0.5068542957305908
+
+# 5
+# simple : 1.5106291770935059
+# load balanced : 0.9076131343841553
+
+# 10
+# simple : 2.763799524307251
+# load balanced : 1.5100840091705323
+
+# 15
+# simple : 4.023359759648641
+# load balanced : 2.1469605445861815
+
+# 20
+# simple : 4.60670313835144
+# load balanced : 2.763711988925934
+
+# 25
+# simple : 6.053423824310303
+# load balanced : 3.3995273876190186
+
+# 30
+# simple : 6.327429262797038
+# load balanced : 4.027852781613668
+
 def doWork(results):
-    while True:
         requests.get()
-        #connection = http.client.HTTPConnection("132.207.12.228", 8080)
-        #start = time.time()
-        #connection.request("GET", "/")
-        #response = connection.getresponse()
-        #end = time.time()
-        results.put(random.random())
+        
+        try:
+            connection = http.client.HTTPConnection("132.207.12.86", 8080)
+            start = time.time()
+            connection.request("GET", "/")
+            response = connection.getresponse()
+            end = time.time()
+            results.put(end - start)
+        except:
+            connection = http.client.HTTPConnection("132.207.12.86", 8080)
+            start = time.time()
+            connection.request("GET", "/")
+            response = connection.getresponse()
+            end = time.time()
+            results.put(end - start)
+
         requests.task_done()
 
-concurrent = 30
+concurrent = 1
 requests = Queue(concurrent)
 results = Queue(concurrent)
 
